b605357fa56eb9f7e4ecd86897a7f01308f60878
Chase Dunagan <cdunagan@utexas.edu>
2016-09-06 00:17:11 -0600

    added playlist crud

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   end
 
   resources :songs, only: [:show]
+  resources :playlists, only: [:index, :new, :create, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
